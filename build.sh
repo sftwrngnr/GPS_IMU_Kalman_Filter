3ddf2a208767f63f0fc1f683cdf8a81d62d11fdd
Dan Henderson <sftwrngnr66@gmail.com>
2022-09-13 20:33:07 -0700

    Added scripts, test and docs directories and files.

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 cd lib
-mkdir blas
 mkdir build
-mkdir demos
-mkdir doc
-mkdir include
-mkdir lapack
-mkdir scripts
-mkdir tests
-mkdir unsupported
 cd build
 cmake ../
+make install
+
